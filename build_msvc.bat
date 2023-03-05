13415e70d2e9ad718555d0b87681697e50df212c
Robin Moalic <robin.moalic@yahoo.fr>
2023-03-05 19:42:54 +0100

    fix: windows build for obs 28+

--- build_msvc.bat
+++ build_msvc.bat
@@ -2,7 +2,7 @@
 
 set CFLAGS=/std:c17 /utf-8 /Zi
 set CXXFLAGS=/std:c++17 /utf-8 /EHsc /Zi
-set INCLUDES=/I . /I obs_studio/libobs
+set INCLUDES=/I . /I obs_studio/libobs /I obs_studio/deps/w32-pthreads
 set LIBS=obs.lib w32-pthreads.lib
 
 del *.obj
@@ -12,4 +12,3 @@ cl.exe %CFLAGS% %INCLUDES% /c list.c obs_media_info.c track_info.c
 
 link.exe /DEBUG:FULL %LIBS% list.obj obs_media_info.obj track_info.obj player_windows_uwp_get_info.obj /DLL /OUT:obs_media_info.dll
 
-copy obs_media_info.* C:\Users\robin\Desktop\OBS-Studio-27.0.1-Full-x64\obs-plugins\64bit\
