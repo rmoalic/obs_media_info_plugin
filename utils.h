64d29b9817ae29594d8bf6e13c667a18400d3b77
Robin Moalic <robin.moalic@yahoo.fr>
2026-05-31 10:06:16 +0200

    fix: removed " - Topic" in artist names from Youtube

--- utils.h
+++ utils.h
@@ -13,4 +13,17 @@
 #define estrcmp(a, b) ((a == NULL || b == NULL) ? 0 : strcmp(a, b))
 #define estrdup(ptr) (ptr == NULL ? NULL : strdup(ptr))
 
+static inline char* strEndsWith(char *str, const char *suffix) {
+    if (str == NULL || suffix == NULL) return NULL;
+    size_t str_len = strlen(str);
+    size_t suffix_len = strlen(suffix);
+    if (suffix_len > str_len) return NULL;
+
+    char* str_suffix_possible_start = str + (str_len - suffix_len);
+    if (strcmp(str_suffix_possible_start, suffix) == 0) {
+        return str_suffix_possible_start;
+    } else {
+        return NULL;
+    }
+}
 #endif
