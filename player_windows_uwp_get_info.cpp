9c3203684cb7ed8bdfda71f91906ba51cb8e66c2
rmoalic <robin.moalic@yahoo.fr>
2026-08-22 18:20:37 +0200

    reverted co_await, fix crash when starting a new player
    Coroutines are more trouble than they are worth.
    Players are sending up to seven event and the ordering is not garanteed by coroutines.
    I tried debouncing and reordering them, there is no ends to the concurency issues.

--- player_windows_uwp_get_info.cpp
+++ player_windows_uwp_get_info.cpp
@@ -31,95 +31,94 @@ void update_players_registration();
 static GlobalSystemMediaTransportControlsSessionManager session_manager { nullptr };
 winrt::event_token session_manager_change_token;
 
-vector<string> registered_players;
-vector<GlobalSystemMediaTransportControlsSession::MediaPropertiesChanged_revoker> session_revoker_media_properties_changed;
-vector<GlobalSystemMediaTransportControlsSession::PlaybackInfoChanged_revoker> session_revoker_playback_ingo_changed;
+struct PlayerSessionData {
+    std::string app_id;
+    GlobalSystemMediaTransportControlsSession::MediaPropertiesChanged_revoker media_properties_revoker;
+    GlobalSystemMediaTransportControlsSession::PlaybackInfoChanged_revoker playback_info_revoker;
+};
+static vector<PlayerSessionData> registered_player_sessions;
 
-static void handle_session_change(GlobalSystemMediaTransportControlsSessionManager session_manager_l, SessionsChangedEventArgs  const& args) {
+static void handle_session_change(GlobalSystemMediaTransportControlsSessionManager session_manager_l, SessionsChangedEventArgs args) {
     update_players_registration();
 }
 
-static winrt::fire_and_forget handle_media_property_change(GlobalSystemMediaTransportControlsSession session, MediaPropertiesChangedEventArgs const& arg) {
+static void handle_media_property_change(GlobalSystemMediaTransportControlsSession session, MediaPropertiesChangedEventArgs arg) {
+    if (session == nullptr) return;
     TrackInfo current_track;
     track_info_struct_init(&current_track);
-    bool playing;
 
-    GlobalSystemMediaTransportControlsSessionMediaProperties media_properties {nullptr};
-
-    winrt::hstring t = session.SourceAppUserModelId();
-    std::string player_t = winrt::to_string(t);
-    const char* player = player_t.c_str();
-
-    media_properties = co_await session.TryGetMediaPropertiesAsync();
-
-    if (media_properties == nullptr) return;
-
+    std::string player;
+    try {
+        player = winrt::to_string(session.SourceAppUserModelId());
+    } catch (...) {
+        return;
+    }
     auto info = session.GetPlaybackInfo();
     if (info == nullptr) return;
 
+    GlobalSystemMediaTransportControlsSessionMediaProperties media_properties{ nullptr };
+
+    try {
+        media_properties = session.TryGetMediaPropertiesAsync().get();
+    } catch (winrt::hresult_error const& e) {
+        log_warning("Error fetching properties: 0x%08X - %s\n", static_cast<uint32_t>(e.code()), winrt::to_string(e.message()).c_str());
+        return;
+    }
+    if (media_properties == nullptr) return;
 
     std::string title = winrt::to_string(media_properties.Title());
-    current_track.title = (char*) title.c_str();
+    current_track.title = const_cast<char*>(title.c_str());
     std::string artist = winrt::to_string(media_properties.Artist());
-    current_track.artist = (char*) artist.c_str();
+    current_track.artist = const_cast<char*>(artist.c_str());
     std::string album = winrt::to_string(media_properties.AlbumTitle());
-    current_track.album = (char*) album.c_str();
-
+    current_track.album = const_cast<char*>(album.c_str());
 
     auto thumbnail = media_properties.Thumbnail();
     com_array<uint8_t> pixel_data_detached;
     uint8_t* data = NULL;
     if (thumbnail != nullptr) {
-        auto stream = co_await thumbnail.OpenReadAsync();
-        auto decoder = co_await BitmapDecoder::CreateAsync(stream);
-        auto transform = BitmapTransform();
-        uint32_t width, height;
-        
-        if (player_t == "Spotify.exe") { // The spotify player has branding on the thumbnail, cropping it out
-            static const int Wcrop = 34;
-            static const int Hcrop = 66;
-            BitmapBounds spotify_bound;
-            width = decoder.PixelWidth() - 2 * Wcrop;
-            height = decoder.PixelHeight() - Hcrop;
+        try {
+            auto stream = thumbnail.OpenReadAsync().get();
+            auto decoder = BitmapDecoder::CreateAsync(stream).get();
+            auto transform = BitmapTransform();
+            uint32_t width = decoder.PixelWidth();
+            uint32_t height = decoder.PixelHeight();
 
-            spotify_bound.X = Wcrop;
-            spotify_bound.Y = 0;
-            spotify_bound.Width = width;
-            spotify_bound.Height = height;
+            auto pixel_data = decoder.GetPixelDataAsync(
+                BitmapPixelFormat::Rgba8,
+                BitmapAlphaMode::Premultiplied,
+                transform,
+                ExifOrientationMode::IgnoreExifOrientation,
+                ColorManagementMode::ColorManageToSRgb
+            ).get();
 
-            transform.Bounds(spotify_bound);
+            pixel_data_detached = pixel_data.DetachPixelData();
+            data = (uint8_t*) pixel_data_detached.data();
 
-        } else {
-            width = decoder.PixelWidth();
-            height = decoder.PixelHeight();
+            current_track.album_art = data;
+            current_track.album_art_size = width * height * 4;
+            current_track.album_art_width = width;
+            current_track.album_art_height = height;
         }
-
-        auto pixel_data = co_await decoder.GetPixelDataAsync(BitmapPixelFormat::Rgba8, 
-                                                      BitmapAlphaMode::Premultiplied,
-                                                      transform,
-                                                      ExifOrientationMode::IgnoreExifOrientation,
-                                                      ColorManagementMode::ColorManageToSRgb);
-        pixel_data_detached = pixel_data.DetachPixelData();
-        data = (uint8_t*) pixel_data_detached.data();
-
-        current_track.album_art = data;
-        current_track.album_art_size = width * height * 4;
-        current_track.album_art_width = width;
-        current_track.album_art_height = height;
+        catch (...) {
+            log_warning("Failed to decode thumbnail stream");
+        }
     }
 
-
-    track_info_register_track_change(player, current_track);
-    
-    track_info_print_players();
+    track_info_register_track_change(player.c_str(), current_track);
 
 }
 
-static void handle_media_playback_info_change(GlobalSystemMediaTransportControlsSession session, PlaybackInfoChangedEventArgs const& args) {
-    
-    winrt::hstring t = session.SourceAppUserModelId();
-    std::string player_t = winrt::to_string(t);
-    const char* player = player_t.c_str();
+static void handle_media_playback_info_change(GlobalSystemMediaTransportControlsSession session, PlaybackInfoChangedEventArgs args)
+{
+    if (session == nullptr) return;
+
+    std::string player;
+    try {
+        player = winrt::to_string(session.SourceAppUserModelId());
+    } catch (...) {
+        return;
+    }
 
     auto info = session.GetPlaybackInfo();
     if (info == nullptr) return;
@@ -127,43 +126,42 @@ static void handle_media_playback_info_change(GlobalSystemMediaTransportControls
     auto status = info.PlaybackStatus();
     bool playing = status == GlobalSystemMediaTransportControlsSessionPlaybackStatus::Playing;
 
-    track_info_register_state_change(player, playing);
-    
-    track_info_print_players();
+    track_info_register_state_change(player.c_str(), playing);
 }
 
 void update_players_registration() {
     vector<string> players_seen;
     auto sessions = session_manager.GetSessions();
-    winrt::hstring AUMI;
 
-    for (auto& session : sessions) {
-        AUMI = session.SourceAppUserModelId();
-        std::string s = winrt::to_string(AUMI);
-        
-        players_seen.push_back(s);
-        if (find(registered_players.begin(), registered_players.end(), s) == registered_players.end()) { // not found
-            auto sr1 = session.MediaPropertiesChanged(winrt::auto_revoke, handle_media_property_change);
-            auto sr2 = session.PlaybackInfoChanged(winrt::auto_revoke, handle_media_playback_info_change);
+    for (auto session : sessions) {
+        std::string AUMI = winrt::to_string(session.SourceAppUserModelId());
+        players_seen.push_back(AUMI);
 
-            session_revoker_media_properties_changed.push_back(std::move(sr1));
-            session_revoker_playback_ingo_changed.push_back(std::move(sr2));
+        auto it = std::find_if(registered_player_sessions.begin(), registered_player_sessions.end(),
+            [&AUMI](const PlayerSessionData& p) { return p.app_id == AUMI; });
 
-            registered_players.push_back(s);
-            track_info_register_player(s.c_str(), s.c_str());
+        if (it == registered_player_sessions.end()) {
+            PlayerSessionData new_player;
+            new_player.app_id = AUMI;
+            new_player.media_properties_revoker = session.MediaPropertiesChanged(winrt::auto_revoke, handle_media_property_change);
+            new_player.playback_info_revoker = session.PlaybackInfoChanged(winrt::auto_revoke, handle_media_playback_info_change);
+
+            registered_player_sessions.push_back(std::move(new_player));
+            track_info_register_player(AUMI.c_str(), AUMI.c_str());
 
             handle_media_property_change(session, NULL);
             handle_media_playback_info_change(session, NULL);
         }
     }
-    sort(registered_players.begin(), registered_players.end());
-    sort(players_seen.begin(), players_seen.end());
-    vector<string> players_not_seen;
-    set_difference(registered_players.begin(), registered_players.end(), players_seen.begin(), players_seen.end(), inserter(players_not_seen, players_not_seen.end()));
 
-    for (string player_name: players_not_seen) {
-        track_info_unregister_player(player_name.c_str());
-        registered_players.erase(std::remove(registered_players.begin(), registered_players.end(), player_name), registered_players.end());
+    auto it = registered_player_sessions.begin();
+    while (it != registered_player_sessions.end()) {
+        if (std::find(players_seen.begin(), players_seen.end(), it->app_id) == players_seen.end()) {
+            track_info_unregister_player(it->app_id.c_str());
+            it = registered_player_sessions.erase(it);
+        } else {
+            ++it;
+        }
     }
 }
 
@@ -176,7 +174,7 @@ extern "C" void player_info_init() {
     try {
         session_manager = GlobalSystemMediaTransportControlsSessionManager::RequestAsync().get();
     } catch (...) {
-        log_error("an error occured while getting the GlobalSystemMediaTransportControlsSessionManager");
+        log_error("an error occurred while getting the GlobalSystemMediaTransportControlsSessionManager");
         return;
     }
     session_manager_change_token = session_manager.SessionsChanged(handle_session_change);
@@ -189,10 +187,12 @@ extern "C" int player_info_process() {
 }
 
 extern "C" void player_info_close() {
-    session_revoker_media_properties_changed.clear();
-    session_revoker_playback_ingo_changed.clear();
-    session_manager.SessionsChanged(session_manager_change_token);
-    session_manager_change_token = {};
+    registered_player_sessions.clear();
+
+    if (session_manager && session_manager_change_token) {
+        session_manager.SessionsChanged(session_manager_change_token);
+        session_manager_change_token = {};
+    }
     session_manager = nullptr;
 
     winrt::uninit_apartment();
